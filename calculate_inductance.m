7a15cbaaefb68a6b9e08f8f632efc4ce6043271b
Syed Muhammad Ali Rizvi <63605517+Ali-Rizvi-1@users.noreply.github.com>
2021-11-21 23:02:14 +0500

    .

--- calculate_inductance.m
+++ calculate_inductance.m
@@ -4,8 +4,9 @@ function [L] = calculate_inductance(r,)
 % 3-phase double circuit
 % L = inductance per conductor per phase
 
-D_s = GMD_self();
-D_m = GMD_mutual();
+D_s = GMD_self(r, d_aA, d_bB, d_cC);
+D_m = GMD_mutual(d_ab,d_aB,d_Ab,d_AB,d_bc,d_bC,d_Bc,...
+    d_BC,d_ca,d_cA,d_Ca,d_CA);
 
 L = 2*10^(-7)*log(D_m/D_s);
 end
