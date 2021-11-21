2d5a01314b1bbfd1a782f7ddc9043153347f283d
Syed Muhammad Ali Rizvi <63605517+Ali-Rizvi-1@users.noreply.github.com>
2021-11-21 23:01:34 +0500

    GMD_mutual function completed

--- GMD_mutual.m
+++ GMD_mutual.m
@@ -1,7 +1,13 @@
-function [D_m] = GMD_mutual(inputArg1,inputArg2)
+function [D_m] = GMD_mutual(d_ab,d_aB,d_Ab,d_AB,d_bc,d_bC,d_Bc,...
+    d_BC,d_ca,d_cA,d_Ca,d_CA)
 %UNTITLED8 Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+D_AB = ( d_ab * d_aB * d_Ab * d_AB )^(1/4) ;
+D_BC = ( d_bc * d_bC * d_Bc * d_BC )^(1/4) ;
+D_CA = ( d_ca * d_cA * d_Ca * d_CA )^(1/4) ;
+
+D_m = (D_AB * D_BC * D_CA)^(1/3) ;
+
 end
 
