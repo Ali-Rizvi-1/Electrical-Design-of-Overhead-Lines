ad33720b1671c2a2bd16f650ed3bc3dd7fb55df0
Syed Muhammad Ali Rizvi <63605517+Ali-Rizvi-1@users.noreply.github.com>
2021-11-21 23:05:13 +0500

    .

--- calculate_inductance.m
+++ calculate_inductance.m
@@ -1,4 +1,4 @@
-function [L] = calculate_inductance(r,)
+function [L] = calculate_inductance(r,d)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 % 3-phase double circuit
@@ -10,4 +10,3 @@ D_m = GMD_mutual(d_ab,d_aB,d_Ab,d_AB,d_bc,d_bC,d_Bc,...
 
 L = 2*10^(-7)*log(D_m/D_s);
 end
-
---
+++ script.m
@@ -0,0 +1,4 @@
+r = 0.75; % radius in cm
+d = [] % distances vector
+
+L = calculate_inductance(r,d);
